9b9677019d4b7857991fe600c1203d23d6227802
ayush kumar <socialacc211@gmail.com>
2024-10-24 19:09:10 +0530

    initial project commit

---
+++ blog-appwrite-react-077/src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div><img src="../../public/blog.png" alt="logo" className='w-60 h-30'  /></div>
+  )
+}
+
+export default Logo
